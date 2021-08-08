aab8d9d258326f15f6f7c840137779deb64195aa
Jorge <jorgeauri@gmail.com>
2021-08-07 21:24:39 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Layout from './components/layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Layout>
+          <div>
+            <img source="https://avatars.githubusercontent.com/u/46933512?v=4" 
+            alt="avatar of user"
+            />
+            <h1>User teste</h1>
+            <h3>Username: </h3>
+            <span>teste</span>
+            <div>
+              <div>
+                <h4>Followers</h4>
+               <span>5</span>
+              </div>
+              <div>
+                <h4>Starreds</h4>
+               <span>5</span>
+              </div>
+              <div>
+                <h4>Followings</h4>
+               <span>5</span>
+              </div>
+            </div>
+          </div>
+        <div>Repositories</div>
+        <div>Starreds</div>
+      </Layout>
+    </main>
   );
 }
 
