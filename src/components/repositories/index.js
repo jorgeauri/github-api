f3f996313c53c35e08f4b4a7e2bd9e7812e02cd2
Jorge <jorgeauri@gmail.com>
2021-08-25 22:48:29 -0300

    fourth commit

--- src/components/repositories/index.js
+++ src/components/repositories/index.js
@@ -5,12 +5,14 @@ const Repositories = () => {
   return (
     <S.WrapperTabs
     selectedTabClassName="is-selected"
-    selectedTabClassName="is-selected"
+    selectedTabPanelClassName="is-selected"
     >
         <S.WrapperTabList>
           <S.WrapperTab>Repositories</S.WrapperTab>
           <S.WrapperTab>Starred</S.WrapperTab>
         </S.WrapperTabList>
+        <S.WrapperTabPanel>panel Repositories</S.WrapperTabPanel>
+        <S.WrapperTabPanel>panel Starred</S.WrapperTabPanel>
     </S.WrapperTabs>
     );
 };
