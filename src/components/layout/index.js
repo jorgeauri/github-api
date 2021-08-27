d08131ba3212007105cc38097732d69ebcf07c02
Jorge <jorgeauri@gmail.com>
2021-08-27 02:16:51 -0300

    sixth commit

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import * as S from "./styled"
+import React from 'react';
+import Header from '../header';
+import * as S from './styled';
 
 const Layout = ({ children }) => {
   return (
     <S.WrapperLayout>
-     <header>header</header> 
-     {children}
+      <Header />
+      {children}
     </S.WrapperLayout>
   );
-}
+};
 
-export default Layout
-
+export default Layout;
--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -17,26 +17,30 @@ const Profile = () => {
           <S.WrapperUserName>
             <h3>Username:</h3>
             <a
-              href="https://github.com/jorgeauri"
+              href={githubState.user.html_url}
               target="_blank"
               rel="noreferrer"
             >
-              jorge
+              {githubState.user.login}
             </a>
           </S.WrapperUserName>
         </div>
         <S.WrapperStatusCount>
           <div>
             <h4>Followers</h4>
-            <span>5</span>
-          </div>
-          <div>
-            <h4>Starred</h4>
-            <span>5</span>
+            <span>{githubState.user.followers}</span>
           </div>
           <div>
             <h4>Followings</h4>
-            <span>5</span>
+            <span>{githubState.user.following}</span>
+          </div>
+          <div>
+            <h4>Gists</h4>
+            <span>{githubState.user.public_gists}</span>
+          </div>
+          <div>
+            <h4>Repos</h4>
+            <span>{githubState.user.public_repos}</span>
           </div>
         </S.WrapperStatusCount>
       </S.WrapperInfoUser>
